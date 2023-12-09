de5c693ea6b31a006d91f2a2c838e905e000900b
DarkLord76865 <mmima191@gmail.com>
2023-12-09 13:56:19 +0100

    update to v2.0.1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Quadratic-Equation-Solver
-A program to solve a quadratic equation
+A simple program to solve quadratic equations
+
 
 ![screenshot](https://user-images.githubusercontent.com/40371578/202571813-69992c35-4025-4ad7-9616-06b2b50ca701.png)
 
@@ -9,6 +10,8 @@ Prebuilt program is available under Releases
 
 ## Building
 1. Clone repository
-2. Run command: `pip install -r requirements.txt`
-3. Run build.py
-4. Built `*.exe` will be placed in the same folder
+2. Install Python
+3. Install dependencies: `pip install -r requirements.txt`
+4. Install UPX (optional)
+5. Run build.py
+6. Built `*.exe` will be placed in the same folder
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from tkinter import *
+import tkinter as tk
 from tkinter.messagebox import showerror
 
 
@@ -13,7 +13,7 @@ def resource_path(relative_path=""):
 		base_path = os.path.abspath(".")
 	return os.path.join(base_path, relative_path)
 
-def rj_kvadratne(a, b, c):
+def rj_kvad(a, b, c):
 	rjesenja = []
 	kompleksna = False
 	if a == 0:
@@ -60,48 +60,11 @@ def rj_kvadratne(a, b, c):
 		elif rjesenja[0] != 0 and rjesenja[1] == 1:
 			ispis = f"X₁ = {rjesenja[0]} + i\nX₂ = {rjesenja[0]} - i"
 		else:
-			ispis = f"X₁ = + i\nX₂ = - i"
+			ispis = "X₁ = + i\nX₂ = - i"
 
 	return ispis
 
-def klik(event=None):
-	global lbl, a, b, c
-
-	err = False
-
-	try:
-		a_val = float(a.get())
-	except ValueError:
-		if a.get() != "":
-			err = True
-			a.delete(0, END)
-		else:
-			a_val = 0
-
-	try:
-		b_val = float(b.get())
-	except ValueError:
-		if b.get() != "":
-			err = True
-			b.delete(0, END)
-		else:
-			b_val = 0
-
-	try:
-		c_val = float(c.get())
-	except ValueError:
-		if c.get() != "":
-			err = True
-			c.delete(0, END)
-		else:
-			c_val = 0
-
-	if err:
-		showerror("Invalid input!", "Invalid number was given!")
-	else:
-		lbl.config(text=rj_kvadratne(a_val, b_val, c_val))
-
-def validate_input(full_text):
+def validate_number(full_text):
 	if " " in full_text or full_text.count(".") > 1:
 		return False
 	elif full_text == "" or full_text == "." or full_text == "-":
@@ -116,49 +79,103 @@ def validate_input(full_text):
 		except ValueError:
 			return False
 
-def convert_change_thickness(event, typ):
-	global convert_btn
-	if typ:
-		convert_btn.config(highlightthickness=1)
-	else:
-		convert_btn.config(highlightthickness=3)
+class App:
+	def __init__(self):
+		self.root = tk.Tk()
+		self.root.geometry(f"500x250"
+		                   f"+{self.root.winfo_screenwidth() // 2 - 250}"
+		                   f"+{self.root.winfo_screenheight() // 2 - 125}")
+		self.root.resizable(False, False)
+		self.root.title("Quadratic Equation Solver")
+		self.root.iconbitmap(resource_path("resources/quad-icon.ico"))
+		self.root.config(background="#202A44")
+
+		self.title = tk.Label(self.root, text="Quadratic Equation Solver", font=("Helvetica", 25, "bold", "italic"),
+		                      borderwidth=0, background="#202A44", activebackground="#202A44", foreground="#ffffff",
+		                      activeforeground="#ffffff", highlightthickness=0)
+		self.title.place(x=0, y=0, width=500, height=80)
+
+		self.jednadzba = tk.Label(self.root, text="          X² +           X +           = 0",
+		                          font=("Arial", 20, "bold"), anchor="center", borderwidth=0, background="#202A44",
+		                          activebackground="#202A44", foreground="#ffffff", activeforeground="#ffffff",
+		                          highlightthickness=0)
+		self.jednadzba.place(x=0, y=92, width=500, height=35)
+
+		self.reg = self.root.register(validate_number)
+
+		self.a = tk.Entry(self.root, justify=tk.CENTER, font=("Arial", 13, "bold"),
+		                  validate="key", validatecommand=(self.reg, "%P"),
+		                  borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green",
+		                  disabledbackground="grey15", disabledforeground="#ffffff", background="grey15",
+		                  foreground="#ffffff", insertbackground="#ffffff")
+		self.b = tk.Entry(self.root, justify=tk.CENTER, font=("Arial", 13, "bold"),
+		                  validate="key", validatecommand=(self.reg, "%P"),
+		                  borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green",
+		                  disabledbackground="grey15", disabledforeground="#ffffff", background="grey15",
+		                  foreground="#ffffff", insertbackground="#ffffff")
+		self.c = tk.Entry(self.root, justify=tk.CENTER, font=("Arial", 13, "bold"),
+		                  validate="key", validatecommand=(self.reg, "%P"),
+		                  borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green",
+		                  disabledbackground="grey15", disabledforeground="#ffffff", background="grey15",
+		                  foreground="#ffffff", insertbackground="#ffffff")
+		self.a.place(x=54, y=90, width=75, height=35)
+		self.b.place(x=193, y=90, width=75, height=35)
+		self.c.place(x=323, y=90, width=75, height=35)
+
+		self.result_lbl = tk.Label(self.root, text="", font=("Arial", 13, "bold"), anchor="center",
+		                           borderwidth=0, background="#202A44", activebackground="#202A44",
+		                           foreground="#ffffff", activeforeground="#ffffff", highlightthickness=0)
+		self.result_lbl.place(x=0, y=130, width=500, height=70)
+
+		self.solve_btn = tk.Label(self.root, text="Solve", font=("Helvetica", 10, "bold"), cursor="hand2",
+		                          highlightthickness=1, highlightbackground="green", highlightcolor="green",
+		                          borderwidth=0, background="grey15", activebackground="grey15", foreground="#ffffff",
+		                          activeforeground="#ffffff")
+		self.solve_btn.place(x=25, y=200, width=450, height=30)
+		self.solve_btn.bind("<Enter>", lambda event: self.solve_btn.config(highlightthickness=3))
+		self.solve_btn.bind("<Leave>", lambda event: self.solve_btn.config(highlightthickness=1))
+		self.solve_btn.bind("<ButtonRelease-1>", lambda event: self.solve_click())
+		self.root.bind("<KeyRelease-Return>", lambda event: self.solve_click())
+
+		self.root.mainloop()
+
+	def solve_click(self):
+		err = False
+
+		try:
+			a_val = float(self.a.get())
+		except ValueError:
+			if self.a.get() != "":
+				err = True
+				self.a.delete(0, tk.END)
+			else:
+				a_val = 0
+
+		try:
+			b_val = float(self.b.get())
+		except ValueError:
+			if self.b.get() != "":
+				err = True
+				self.b.delete(0, tk.END)
+			else:
+				b_val = 0
+
+		try:
+			c_val = float(self.c.get())
+		except ValueError:
+			if self.c.get() != "":
+				err = True
+				self.c.delete(0, tk.END)
+			else:
+				c_val = 0
+
+		if err:
+			showerror("Invalid input!", "Invalid number was given!")
+		else:
+			self.result_lbl.config(text=rj_kvad(a_val, b_val, c_val))
 
 def main():
-	global convert_btn
-	global lbl, a, b, c
-
-	root = Tk()
-	root.geometry(f"500x250+{root.winfo_screenwidth() // 2 - 250}+{root.winfo_screenheight() // 2 - 125}")
-	root.resizable(False, False)
-	root.title("Quadratic Equation Solver")
-	root.iconbitmap(resource_path("data/quad-eq-solver-icon.ico"))
-	root.config(background="#202A44")
-
-	title = Label(root, text="Quadratic Equation Solver", font=("Helvetica", 25, "bold", "italic"), borderwidth=0, background="#202A44", activebackground="#202A44", foreground="#ffffff", activeforeground="#ffffff", highlightthickness=0)
-	title.place(x=0, y=0, width=500, height=80)
-
-	jednadzba = Label(root, text="          X² +           X +           = 0", font=("Arial", 20, "bold"), anchor="center", borderwidth=0, background="#202A44", activebackground="#202A44", foreground="#ffffff", activeforeground="#ffffff", highlightthickness=0)
-	jednadzba.place(x=0, y=90, width=500, height=35)
-	reg = root.register(validate_input)
-
-	a = Entry(root, justify=CENTER, validate="key", validatecommand=(reg, "%P"), borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green", disabledbackground="grey15", disabledforeground="#ffffff", background="grey15", foreground="#ffffff", insertbackground="#ffffff")
-	b = Entry(root, justify=CENTER, validate="key", validatecommand=(reg, "%P"), borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green", disabledbackground="grey15", disabledforeground="#ffffff", background="grey15", foreground="#ffffff", insertbackground="#ffffff")
-	c = Entry(root, justify=CENTER, validate="key", validatecommand=(reg, "%P"), borderwidth=0, highlightthickness=1, highlightbackground="green", highlightcolor="green", disabledbackground="grey15", disabledforeground="#ffffff", background="grey15", foreground="#ffffff", insertbackground="#ffffff")
-	lbl = Label(root, text="", font=("Arial", 13, "bold"), anchor="center", borderwidth=0, background="#202A44", activebackground="#202A44", foreground="#ffffff", activeforeground="#ffffff", highlightthickness=0)
-	a.place(x=54, y=90, width=75, height=35)
-	b.place(x=193, y=90, width=75, height=35)
-	c.place(x=323, y=90, width=75, height=35)
-
-	convert_btn = Label(root, text="Solve", font=("Helvetica", 10, "bold"), highlightthickness=1, highlightbackground="green", highlightcolor="green", borderwidth=0, background="grey15", activebackground="grey15", foreground="#ffffff", activeforeground="#ffffff")
-	convert_btn.place(x=25, y=215, width=450, height=30)
-	convert_btn.bind("<Enter>", lambda event: convert_change_thickness(event, False))
-	convert_btn.bind("<Leave>", lambda event: convert_change_thickness(event, True))
-	convert_btn.bind("<ButtonRelease-1>", klik)
-
-	root.bind("<KeyRelease-Return>", klik)
-
-	lbl.place(x=0, y=130, width=500, height=90)
-	root.mainloop()
+	App()
 
 
 if __name__ == '__main__':
